47ea50d83099df767c68375fd1c6cf806478a437
fred <1301078128@qq.com>
2017-10-24 12:42:37 +0800

    基本样式

---
+++ src/app.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class='abc'>
+     <!-- {{name}}  -->
+    <!-- <el-button type="primary">主要按钮</el-button> -->
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+  export default{
+    data(){
+      return{
+        name:'open亮音000'
+      }
+    },
+    methods: {
+        
+    }
+    }
+  
+</script>
+<style>
+  .abc{
+    color: green;
+  }
+</style>
+
