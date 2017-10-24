8138a4bc8fb09d9fc1da136254a64925fe40ac8b
fred <1301078128@qq.com>
2017-10-24 17:02:32 +0800

    分类栏完成

--- src/components/site/goodslist.vue
+++ src/components/site/goodslist.vue
@@ -1,215 +1,293 @@
 <template>
-<div class="tmpl">
-  <!--面包屑  -->
-  <div class="section">
-<div class="location">
-<span>当前位置：</span>
-<a href="/index.html">首页</a> &gt;
-<a href="/goods.html">购物商城</a>
-</div>
-</div>
+  <div class="tmpl">
+    <!--面包屑  -->
+    <div class="section">
+      <div class="location">
+        <span>当前位置：</span>
+        <a href="/index.html">首页</a> &gt;
+        <a href="/goods.html">购物商城</a>
+      </div>
+    </div>
 
-<!--左侧栏,轮播图,top顶栏  -->
+    <!--左侧栏,轮播图,top顶栏  -->
 
 
-<div class="section">
-    <div class="wrapper">
+    <div class="section">
+      <div class="wrapper">
         <div class="wrap-box">
-            <!--类别菜单-->
-            <div class="left-220" style="margin:0;">
-                <div class="banner-nav">
-                    <ul>
-                    <!--此处声明下面可重复循环-->
-                     
-                        <li>
-                            <h3>
-                                <i class="iconfont icon-arrow-right"></i>
-                                <span>手机数码</span>
-                                <p>
-                                 
-                                    手机通讯
-                                 
-                                    摄影摄像
-                                 
-                                    存储设备
-                                 
-                                </p>
-                            </h3>
-                            <div class="item-box">
-                                <!--如有三级分类，此处可循环-->
-                                <dl>
-                                    <dt><a href="/goods/40.html">手机数码</a></dt>
-                                    <dd>
-                                         
-                                        <a href="/goods/43.html">手机通讯</a>
-                                         
-                                        <a href="/goods/44.html">摄影摄像</a>
-                                         
-                                        <a href="/goods/45.html">存储设备</a>
-                                         
-                                    </dd>
-                                </dl>
-                            </div>
-                        </li>
-                     
-                        <li>
-                            <h3>
-                                <i class="iconfont icon-arrow-right"></i>
-                                <span>电脑办公</span>
-                                <p>
-                                 
-                                    电脑整机
-                                 
-                                    外设产品
-                                 
-                                    办公打印
-                                 
-                                </p>
-                            </h3>
-                            <div class="item-box">
-                                <!--如有三级分类，此处可循环-->
-                                <dl>
-                                    <dt><a href="/goods/41.html">电脑办公</a></dt>
-                                    <dd>
-                                         
-                                        <a href="/goods/46.html">电脑整机</a>
-                                         
-                                        <a href="/goods/47.html">外设产品</a>
-                                         
-                                        <a href="/goods/48.html">办公打印</a>
-                                         
-                                    </dd>
-                                </dl>
-                            </div>
-                        </li>
-                     
-                        <li>
-                            <h3>
-                                <i class="iconfont icon-arrow-right"></i>
-                                <span>影音娱乐</span>
-                                <p>
-                                 
-                                    平板电视
-                                 
-                                    音响DVD
-                                 
-                                    影音配件
-                                 
-                                </p>
-                            </h3>
-                            <div class="item-box">
-                                <!--如有三级分类，此处可循环-->
-                                <dl>
-                                    <dt><a href="/goods/42.html">影音娱乐</a></dt>
-                                    <dd>
-                                         
-                                        <a href="/goods/49.html">平板电视</a>
-                                         
-                                        <a href="/goods/50.html">音响DVD</a>
-                                         
-                                        <a href="/goods/51.html">影音配件</a>
-                                         
-                                    </dd>
-                                </dl>
-                            </div>
-                        </li>
-                     
-                    </ul>
-                </div>
+          <!--类别菜单-->
+          <div class="left-220" style="margin:0;">
+            <div class="banner-nav">
+              <ul>
+                <!--此处声明下面可重复循环-->
+
+                <li v-for="(item,index) in ginfo.catelist" :key="index">
+                  <h3>
+                    <i class="iconfont icon-arrow-right"></i>
+                    <span>{{item.title}}</span>
+                    <p>
+                      <span v-for="subitem in item.subcates">{{subitem.title}}</span>
+                    </p>
+                  </h3>
+                  <div class="item-box">
+                    <!--如有三级分类，此处可循环-->
+                    <dl>
+                      <!--一级分类  -->
+                      <dt><a href="/goods/40.html">{{item.title}}</a></dt>
+                      <dd>
+                        <!--二级分类  -->
+                        <a v-for="subitem in item.subcates" href="/goods/43.html">{{subitem.title}}</a>
+
+                      </dd>
+                    </dl>
+                  </div>
+                </li>
+              </ul>
             </div>
-            <!--/类别菜单-->
-             
-            <!--幻灯片-->
-            <div class="left-705">
-                <div class="banner-img">
-                    <div id="focus-box" class="focus-box">
-                        <ul class="slides">
-                            <li class="" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 0; display: block; z-index: 1;">
-                                <a href="/goods.html">
+          </div>
+          <!--/类别菜单-->
+
+          <!--幻灯片-->
+          <div class="left-705">
+            <div class="banner-img">
+              <div id="focus-box" class="focus-box">
+                <ul class="slides">
+                  <li class="" style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 0; display: block; z-index: 1;">
+                    <a href="/goods.html">
                                     <img src="/templates/main/images/focus_1.png" draggable="false">
                                 </a>
-                            </li>
-                            <li style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;" class="flex-active-slide">
-                                <a href="/goods.html">
+                  </li>
+                  <li style="width: 100%; float: left; margin-right: -100%; position: relative; opacity: 1; display: block; z-index: 2;" class="flex-active-slide">
+                    <a href="/goods.html">
                                     <img src="/templates/main/images/focus_2.png" draggable="false">
                                 </a>
-                            </li>
-                        </ul>
-                    <ol class="flex-control-nav flex-control-paging"><li><a class="">1</a></li><li><a class="flex-active">2</a></li></ol></div>
-                     
-                </div>
-            </div>
-            <!--/幻灯片-->
-             
-            <!--推荐商品-->
-            <div class="left-220">
-                <ul class="side-img-list">
-                 
-                    <li>
-                        <div class="img-box">
-                            <label>1</label>
-                            <img src="/upload/201504/20/thumb_201504200314272543.jpg">
-                        </div>
-                        <div class="txt-box">
-                            <a href="/goods/show-98.html">奔腾（BNTN） 380功放+纽约至尊 套装家庭影院</a>
-                            <span>2015-04-20</span>
-                        </div>
-                    </li>
-                 
-                    <li>
-                        <div class="img-box">
-                            <label>2</label>
-                            <img src="/upload/201504/20/thumb_201504200258403759.jpg">
-                        </div>
-                        <div class="txt-box">
-                            <a href="/goods/show-97.html">三星（SAMSUNG）UA40HU5920JXXZ 40英寸4K超高清</a>
-                            <span>2015-04-20</span>
-                        </div>
-                    </li>
-                 
-                    <li>
-                        <div class="img-box">
-                            <label>3</label>
-                            <img src="/upload/201504/20/thumb_201504200242250674.jpg">
-                        </div>
-                        <div class="txt-box">
-                            <a href="/goods/show-95.html">惠普（HP）LaserJet 2035商用黑白激光打印机（黑色）</a>
-                            <span>2015-04-20</span>
-                        </div>
-                    </li>
-                 
-                    <li>
-                        <div class="img-box">
-                            <label>4</label>
-                            <img src="/upload/201504/20/thumb_201504200239192345.jpg">
-                        </div>
-                        <div class="txt-box">
-                            <a href="/goods/show-94.html">金士顿（Kingston） DataTraveler SE9 32GB 金属U盘</a>
-                            <span>2015-04-20</span>
-                        </div>
-                    </li>
-                 
+                  </li>
                 </ul>
+                <ol class="flex-control-nav flex-control-paging">
+                  <li><a class="">1</a></li>
+                  <li><a class="flex-active">2</a></li>
+                </ol>
+              </div>
+
             </div>
-            <!--/推荐商品-->
+          </div>
+          <!--/幻灯片-->
+
+          <!--推荐商品-->
+          <div class="left-220">
+            <ul class="side-img-list">
+
+              <li>
+                <div class="img-box">
+                  <label>1</label>
+                  <img src="/upload/201504/20/thumb_201504200314272543.jpg">
+                </div>
+                <div class="txt-box">
+                  <a href="/goods/show-98.html">奔腾（BNTN） 380功放+纽约至尊 套装家庭影院</a>
+                  <span>2015-04-20</span>
+                </div>
+              </li>
+
+              <li>
+                <div class="img-box">
+                  <label>2</label>
+                  <img src="/upload/201504/20/thumb_201504200258403759.jpg">
+                </div>
+                <div class="txt-box">
+                  <a href="/goods/show-97.html">三星（SAMSUNG）UA40HU5920JXXZ 40英寸4K超高清</a>
+                  <span>2015-04-20</span>
+                </div>
+              </li>
+
+              <li>
+                <div class="img-box">
+                  <label>3</label>
+                  <img src="/upload/201504/20/thumb_201504200242250674.jpg">
+                </div>
+                <div class="txt-box">
+                  <a href="/goods/show-95.html">惠普（HP）LaserJet 2035商用黑白激光打印机（黑色）</a>
+                  <span>2015-04-20</span>
+                </div>
+              </li>
+
+              <li>
+                <div class="img-box">
+                  <label>4</label>
+                  <img src="/upload/201504/20/thumb_201504200239192345.jpg">
+                </div>
+                <div class="txt-box">
+                  <a href="/goods/show-94.html">金士顿（Kingston） DataTraveler SE9 32GB 金属U盘</a>
+                  <span>2015-04-20</span>
+                </div>
+              </li>
+
+            </ul>
+          </div>
+          <!--/推荐商品-->
         </div>
+      </div>
     </div>
-</div>
- 
 
 
-</div>
+    <!--下半部分  -->
+
+   
+
+    <div class="section">
+
+      <!--子类-->
+      <div class="main-tit">
+        <h2>手机数码</h2>
+        <p>
+
+          <a href="/goods/43.html">手机通讯</a>
+
+          <a href="/goods/44.html">摄影摄像</a>
+
+          <a href="/goods/45.html">存储设备</a>
+
+          <a href="/goods/40.html">更多
+<i>+</i>
+</a>
+        </p>
+      </div>
+      <!--/子类-->
+      <div class="wrapper clearfix">
+        <div class="wrap-box">
+          <ul class="img-list">
+
+            <li>
+              <a href="/goods/show-91.html">
+                <div class="img-box">
+                  <img src="/upload/201504/20/thumb_201504200214471783.jpg">
+                </div>
+                <div class="info">
+                  <h3>尼康(Nikon)D3300套机（18-55mm f/3.5-5.6G VRII）（黑色）</h3>
+                  <p class="price">
+                    <b>¥3180.00</b>元</p>
+                  <p>
+                    <strong>库存 10</strong>
+                    <span>市场价：
+<s>3150.00</s>
+</span>
+                  </p>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="/goods/show-90.html">
+                <div class="img-box">
+                  <img src="/upload/201504/20/thumb_201504200154277661.jpg">
+                </div>
+                <div class="info">
+                  <h3>佳能（Canon） EOS 700D 单反套机</h3>
+                  <p class="price">
+                    <b>¥4799.00</b>元</p>
+                  <p>
+                    <strong>库存 98</strong>
+                    <span>市场价：
+<s>5099.00</s>
+</span>
+                  </p>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="/goods/show-89.html">
+                <div class="img-box">
+                  <img src="/upload/201504/20/thumb_201504200119256512.jpg">
+                </div>
+                <div class="info">
+                  <h3>小米（Mi）小米Note 16G双网通版</h3>
+                  <p class="price">
+                    <b>¥2299.00</b>元</p>
+                  <p>
+                    <strong>库存 58</strong>
+                    <span>市场价：
+<s>2699.00</s>
+</span>
+                  </p>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="/goods/show-88.html">
+                <div class="img-box">
+                  <img src="/upload/201504/20/thumb_201504200059017695.jpg">
+                </div>
+                <div class="info">
+                  <h3>苹果Apple iPhone 6 Plus 16G 4G手机（联通三网版）</h3>
+                  <p class="price">
+                    <b>¥5780.00</b>元</p>
+                  <p>
+                    <strong>库存 198</strong>
+                    <span>市场价：
+<s>6388.00</s>
+</span>
+                  </p>
+                </div>
+              </a>
+            </li>
+
+            <li>
+              <a href="/goods/show-87.html">
+                <div class="img-box">
+                  <img src="/upload/201504/20/thumb_201504200046589514.jpg">
+                </div>
+                <div class="info">
+                  <h3>华为（HUAWEI）荣耀6Plus 16G双4G版</h3>
+                  <p class="price">
+                    <b>¥2195.00</b>元</p>
+                  <p>
+                    <strong>库存 60</strong>
+                    <span>市场价：
+<s>2499.00</s>
+</span>
+                  </p>
+                </div>
+              </a>
+            </li>
+
+          </ul>
+        </div>
+      </div>
+    </div>
+
+  </div>
 </template>
 
 <script>
- export default {
- data() {
- return{
- }
- },
- methods:{
- }
- }
+  export default {
+    data() {
+      return {
+         // 定义一个对象 ginfo 用来存储接口：/site/goods/gettopdata/goods返回的message对象
+        ginfo:{},
+        clist:[]  // 用来存储接口site/goods/getgoodsgroup 中返回的messsage数据
+      }
+    },
+    created(){
+      this.getginfo();
+    },
+    methods: {
+      getginfo(){
+        this.$http.get('/site/goods/gettopdata/goods').then(res=>{
+          
+            this.ginfo=res.data.message;
+          
+        });
+      },
+      getclist(){
+        this.$http.get('/site/goods/getgoodsgroup').then(res=>{
+          this.clist=res.data.message;
+        })
+      }
+    }
+  }
+
 </script>
 <style scoped>
+
 </style>
